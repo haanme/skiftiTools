f866d8a435c54057b0c1f3cd6a16bdec9628c3fc
haanme <haanme@utu.fi>
2023-08-18 19:02:18 +0300

    fix for rtd

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'myst_parser',
+              #    'myst_parser',
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
