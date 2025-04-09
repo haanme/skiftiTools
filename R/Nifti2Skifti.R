bc632775347d401f9a190c09d851f18585cba13c
Harri Merisaari <haanme@utu.fi>
2025-04-09 13:04:18 +0300

    Update Nifti2Skifti.R fixed print messages to CRAN compatible

--- R/Nifti2Skifti.R
+++ R/Nifti2Skifti.R
@@ -20,12 +20,13 @@ library(RNifti)
 #' @param Nifti_data Intensity data in Nifti format
 #' @param Nifti_skeleton Skeleton at same imaging space as the data, in Nifti format
 #' @param selected_volumes Selected volume indexes starting from 1
+#' @param verbose TRUE/FALSE(default) for verbose messages
 #' 
 #' @return skifti object with default rownames as vol1, vol2 .... volN as indexes from the nifti data
 #' @importFrom RNifti niftiHeader readNifti
 #' @export
 #'
-Nifti2Skifti <- function(Nifti_data=NULL, Nifti_skeleton=NULL, selected_volumes=NULL) {
+Nifti2Skifti <- function(Nifti_data=NULL, Nifti_skeleton=NULL, selected_volumes=NULL, verbose=FALSE) {
 
   if (is.null(Nifti_data)) {
     warning("`Nifti_data`, was NULL: Nothing to read!\n")
@@ -44,18 +45,18 @@ Nifti2Skifti <- function(Nifti_data=NULL, Nifti_skeleton=NULL, selected_volumes=
     warning(paste("`Nifti_skeleton`, does not exist:", Nifti_skeleton, "\n", sep=''))
     return(NULL)
   }
-  print(paste("Reading " , Nifti_data, sep=""))
+  if(verbose) {
+    print(paste("Reading " , Nifti_data, sep=""))    
+  }
   mask<-RNifti::readNifti(Nifti_skeleton, internal = FALSE, volumes = NULL)
   img_hdr<-RNifti::niftiHeader(Nifti_data)
   mask_hdr<-niftiHeader(mask)
   for(i in 1:3) {
     if(img_hdr$dim[i] != img_hdr$dim[i]){
-      print(paste('Dimensions of data and skeleton mask do not match ', img_hdr$dim[i], img_hdr$dim[i], sep=' '))
-      return(NULL)
+      stop(paste('Dimensions of data and skeleton mask do not match ', img_hdr$dim[i], img_hdr$dim[i], sep=' '))
     }
     if(img_hdr$pixdim[i] != img_hdr$pixdim[i]){
-      print(paste('Dimensions of data and skeleton mask do not match ', img_hdr$pixdim[i], img_hdr$pixdim[i], sep=' '))
-      return(NULL)
+      stop(paste('Dimensions of data and skeleton mask do not match ', img_hdr$pixdim[i], img_hdr$pixdim[i], sep=' '))
     }
   }
 
@@ -72,21 +73,27 @@ Nifti2Skifti <- function(Nifti_data=NULL, Nifti_skeleton=NULL, selected_volumes=
     } else {
       ii<-selected_volumes[i]
       if(ii < 1 | ii > img_hdr$dim[5]) {
-        print(paste('Selected volume index ', ii, ' out of bounds [1..', img_hdr$dim[5], ']', sep=' '))
+        if(verbose) {
+          print(paste('Selected volume index ', ii, ' out of bounds [1..', img_hdr$dim[5], ']', sep=' '))
+        }        
         return(NULL)
       }
     }
     names[[length(names)+1]]<-c(paste("vol", ii, sep=''))
     img<-RNifti::readNifti(Nifti_data, internal = FALSE, volumes = c(ii))
     rowdata<-img[mask>0]
-    cat(paste("\r Reading Nifti data ", i, "/", total_volumes_to_read, " ", names[[length(names)]], sep=""))
+    if(verbose) {
+      cat(paste("\r Reading Nifti data ", i, "/", total_volumes_to_read, " ", names[[length(names)]], sep=""))
+    }
     if(i==1) {
       data<-data.frame(rowdata)
     } else {
       data<-cbind(data, rowdata)
     }
   }
-  print("")
+  if(verbose) {
+    print("")
+  }
   data<-t(data)
   rownames(data)<-names
   skifti<-list(reftype="filename", refdata=Nifti_skeleton, dim=mask_hdr$dim, pixdim=mask_hdr$pixdim, xform=rbind(mask_hdr$srow_x,mask_hdr$srow_y,mask_hdr$srow_z), datatype=NULL, version='0.1', data=data)
